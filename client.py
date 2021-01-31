5db30b7780c96c20ab6b2309575afdbfa1e0e811
fatjonxhaka <78213991+fatjonxhaka@users.noreply.github.com>
2021-01-31 20:10:44 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,19 @@
+import socket 
+import threading 
+from tkinter import *
+from tkinter import font 
+from tkinter import ttk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import db
 
+
+class ChatGeneral:
+    ### Informacionet kryesore 
+    PORT = 5080
+    SERVER = "127.0.0.1"
+    ADDRESS = (SERVER, PORT) 
+    FORMAT = "utf-8"
+    
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    client.connect(ADDRESS) 

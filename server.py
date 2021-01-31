fe8f4aa9dcee121e44a1708974af6a15f822ca3f
fatjonxhaka <78213991+fatjonxhaka@users.noreply.github.com>
2021-01-31 17:45:45 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,20 @@
 
+import socket  
+import threading
+  
+
+class Communicate:
+
+    def __init__(self):
+
+        self.PORT = 5080
+        self.SERVER = '127.0.0.1'   
+        #self.SERVER = socket.gethostbyname(socket.gethostname())	  
+        self.ADDRESS = (self.SERVER, self.PORT)  
+        self.FORMAT = "utf-8"
+        self.clients, self.names = [], [] 
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.server.bind(self.ADDRESS)
+
+    def startChat(self):   ### Polimorfizem
+        print(self.SERVER + " has not started!!") 

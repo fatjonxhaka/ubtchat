e9e9a6d3d8aeed6c2b6288b8e67ccf6fbc64504e
ledion <ledion_ledi@live.com>
2021-01-31 17:39:11 +0100

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,1 +1,23 @@
+from tkinter import *
+import client
 
+
+class WelcomeWindow:
+    
+    def __init__(self):
+        # Krijimi i faqes se pare
+        self.win = Tk()
+
+        #vendosja e dritares dhe ngjyres se prapavise
+        self.canvas = Canvas(self.win, width=600, height=400, bg='black')
+        self.canvas.pack(expand=YES, fill=BOTH)
+    
+    def __str__(self):
+  
+        #shfaqja e dritares ne qender te ekranit
+        width = self.win.winfo_screenwidth()
+        height = self.win.winfo_screenheight()
+        x = int(width / 2 - 600 / 2)
+        y = int(height / 2 - 400 / 2)
+        str1 = "500x500+"+ (x) + "+" + (y)
+        self.win.geometry(str1)

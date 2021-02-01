c03a7a3e0c74e022d6fd2786294034a903a618ed
ledion <ledion_ledi@live.com>
2021-02-01 16:30:24 +0100

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -21,3 +21,29 @@ class WelcomeWindow:
         y = int(height / 2 - 400 / 2)
         str1 = "500x500+"+ (x) + "+" + (y)
         self.win.geometry(str1)
+
+        
+class CreateFrame(WelcomeWindow):
+
+    def __init__(self):
+        
+        self.win = Tk()
+
+        
+        self.canvas = Canvas(self.win, width=600, height=400, bg='#359fd2')
+        self.canvas.pack(expand=YES, fill=BOTH)
+   
+         
+        
+        width = self.win.winfo_screenwidth()
+        height = self.win.winfo_screenheight()
+        x = int(width / 2 - 600 / 2)
+        y = int(height / 2 - 400 / 2)
+        str1 = "600x400+"+ str(x) + "+" + str(y)
+        self.win.geometry(str1)
+
+        #çaktivizimi i ndryshimit te madhesise se dritares
+        self.win.resizable(width=False, height=False)
+
+        #ndryshimi i titullit te dritares
+        self.win.title("UBT Students Point")

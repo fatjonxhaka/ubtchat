762a593b6d61c1b948b22b020084475e8cb92e55
florianemushica <78215485+florianemushica@users.noreply.github.com>
2021-02-03 15:30:40 +0100

    Update db.py

--- db.py
+++ db.py
@@ -6,3 +6,20 @@ class Database(ABC):
     @abstractmethod
     def dbcreate(self):
         pass
+
+class DatabaseLink(Database):
+    #krijimi i  funksionit per lidhje me mysqlserver-in
+
+    def dbcreate(self, tup):
+        self.connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="ubtchat"
+        )
+        self.cursor = self.connection.cursor()  
+        try:
+            self.cursor.execute("SELECT * FROM `ubtchatlist` WHERE `Perdoruesi`=%s AND `Fjalekalimi`=%s",tup)
+            return (self.cursor.fetchone())
+        except:
+            return False
